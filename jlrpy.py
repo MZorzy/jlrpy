d7548689999bf11250898999d9f178d3bd5fff23
MZorzy <mzorzy@gmail.com>
2019-11-01 11:53:10 +0100

    Update jlrpy.py

--- jlrpy.py
+++ jlrpy.py
@@ -121,6 +121,7 @@ class Connection(object):
             "X-Device-Id": self.device_id,
             "Content-Type": "application/json"}
 
+#1 POST auth 
     def __authenticate(self, data=None):
         """Raw urlopen command to the auth url"""
         url = "%s/tokens" % IFAS_BASE_URL
@@ -131,6 +132,7 @@ class Connection(object):
 
         return self.__open(url, auth_headers, data)
 
+#2 POST device registration
     def __register_device(self, headers=None):
         """Register the device Id"""
         url = "%s/users/%s/clients" % (IFOP_BASE_ULR, self.email)
@@ -143,6 +145,7 @@ class Connection(object):
 
         return self.__open(url, headers, data)
 
+#3 GET login user
     def __login_user(self, headers=None):
         """Login the user"""
         url = "%s/users?loginName=%s" % (IF9_BASE_URL, self.email)
@@ -153,6 +156,7 @@ class Connection(object):
         self.user_id = user_data['userId']
         return user_data
 
+#
     def refresh_tokens(self):
         """Refresh tokens."""
         self.oauth = {
@@ -165,21 +169,25 @@ class Connection(object):
         logger.info("[+] Tokens refreshed")
         self.__register_device_and_log_in()
 
+#4 GET get vehicles for user id 
     def get_vehicles(self, headers):
         """Get vehicles for user"""
         url = "%s/users/%s/vehicles?primaryOnly=true" % (IF9_BASE_URL, self.user_id)
         return self.__open(url, headers)
 
+#
     def get_user_info(self):
         """Get user information"""
         return self.get(self.user_id, "%s/users" % IF9_BASE_URL, self.head)
 
+#
     def update_user_info(self, user_info_data):
         """Update user information"""
         headers = self.head.copy()
         headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.User-v3+json; charset=utf-8"
         return self.post(self.user_id, "%s/users" % IF9_BASE_URL, headers, user_info_data)
 
+#
     def reverse_geocode(self, lat, lon):
         """Get geocode information"""
         return self.get("en",
@@ -200,6 +208,7 @@ class Vehicle(dict):
         self.connection = connection
         self.vin = data['vin']
 
+#
     def get_attributes(self):
         """Get vehicle attributes"""
         headers = self.connection.head.copy()
@@ -207,6 +216,7 @@ class Vehicle(dict):
         result = self.get('attributes', headers)
         return result
 
+# GET get vehicle status 
     def get_status(self, key=None):
         """Get vehicle status"""
         headers = self.connection.head.copy()
@@ -218,6 +228,7 @@ class Vehicle(dict):
 
         return result
 
+# POST get vehicle health status 
     def get_health_status(self):
         """Get vehicle health status"""
         headers = self.connection.head.copy()
@@ -228,49 +239,58 @@ class Vehicle(dict):
 
         return self.post('healthstatus', headers, vhs_data)
 
+#
     def get_departure_timers(self):
         """Get vehicle departure timers"""
         headers = self.connection.head.copy()
         headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.DepartureTimerSettings-v1+json"
         return self.get("departuretimers", headers)
 
+#
     def get_wakeup_time(self):
         """Get configured wakeup time for vehicle"""
         headers = self.connection.head.copy()
         headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.VehicleWakeupTime-v2+json"
         return self.get("wakeuptime", headers)
 
+#
     def get_subscription_packages(self):
         """Get vehicle status"""
         result = self.get('subscriptionpackages', self.connection.head)
         return result
 
+#
     def get_trips(self, count=1000):
         """Get the last 1000 trips associated with vehicle"""
         headers = self.connection.head.copy()
         headers["Accept"] = "application/vnd.ngtp.org.triplist-v2+json"
         return self.get('trips?count=%d' % count, headers)
 
+#
     def get_trip(self, trip_id):
         """Get info on a specific trip"""
         return self.get('trips/%s/route?pageSize=1000&page=0' % trip_id, self.connection.head)
 
+#GET get vehicle position 
     def get_position(self):
         """Get current vehicle position"""
         return self.get('position', self.connection.head)
 
+#
     def get_service_status(self, service_id):
         """Get service status"""
         headers = self.connection.head.copy()
         headers["Accept"] = "application/vnd.wirelesscar.ngtp.if9.ServiceStatus-v4+json"
         return self.get('services/%s' % service_id, headers)
 
+#
     def set_attributes(self, nickname, registration_number):
         """Set vehicle nickname and registration number"""
         attributes_data = {"nickname": nickname,
                            "registrationNumber": registration_number}
         return self.post("attributes", self.connection.head, attributes_data)
 
+#
     def lock(self, pin):
         """Lock vehicle. Requires personal PIN for authentication"""
         headers = self.connection.head.copy()
@@ -279,6 +299,7 @@ class Vehicle(dict):
 
         return self.post("lock", headers, rdl_data)
 
+#
     def unlock(self, pin):
         """Unlock vehicle. Requires personal PIN for authentication"""
         headers = self.connection.head.copy()
@@ -287,6 +308,7 @@ class Vehicle(dict):
 
         return self.post("unlock", headers, rdu_data)
 
+#
     def reset_alarm(self, pin):
         """Reset vehicle alarm"""
         headers = self.connection.head.copy()
@@ -296,6 +318,7 @@ class Vehicle(dict):
 
         return self.post("unlock", headers, aloff_data)
 
+#
     def honk_blink(self):
         """Sound the horn and blink lights"""
         headers = self.connection.head.copy()
@@ -305,6 +328,7 @@ class Vehicle(dict):
         hblf_data = self.authenticate_hblf()
         return self.post("honkBlink", headers, hblf_data)
 
+#
     def preconditioning_start(self, target_temp):
         """Start pre-conditioning for specified temperature (celsius)"""
         service_parameters = [{"key": "PRECONDITIONING",
@@ -313,18 +337,21 @@ class Vehicle(dict):
                                "value": "%s" % target_temp}]
         return self._preconditioning_control(service_parameters)
 
+#
     def preconditioning_stop(self):
         """Stop climate preconditioning"""
         service_parameters = [{"key": "PRECONDITIONING",
                                "value": "STOP"}]
         return self._preconditioning_control(service_parameters)
 
+#
     def climate_prioritize(self, priority):
         """Optimize climate controls for comfort or range"""
         service_parameters = [{"key": "PRIORITY_SETTING",
                                "value": "%s" % priority}]
         return self._preconditioning_control(service_parameters)
 
+#
     def _preconditioning_control(self, service_parameters):
         """Control the climate preconditioning"""
         headers = self.connection.head.copy()
@@ -336,6 +363,7 @@ class Vehicle(dict):
 
         return self.post("preconditioning", headers, ecc_data)
 
+#
     def charging_stop(self):
         """Stop charging"""
         service_parameters = [{"key": "CHARGE_NOW_SETTING",
@@ -343,6 +371,7 @@ class Vehicle(dict):
 
         return self._charging_profile_control("serviceParameters", service_parameters)
 
+#
     def charging_start(self):
         """Start charging"""
         service_parameters = [{"key": "CHARGE_NOW_SETTING",
@@ -350,6 +379,7 @@ class Vehicle(dict):
 
         return self._charging_profile_control("serviceParameters", service_parameters)
 
+#
     def set_max_soc(self, max_charge_level):
         """Set max state of charge in percentage"""
         service_parameters = [{"key": "SET_PERMANENT_MAX_SOC",
@@ -357,6 +387,7 @@ class Vehicle(dict):
 
         return self._charging_profile_control("serviceParameters", service_parameters)
 
+#
     def set_one_off_max_soc(self, max_charge_level):
         """Set one off max state of charge in percentage"""
         service_parameters = [{"key": "SET_ONE_OFF_MAX_SOC",
@@ -364,6 +395,7 @@ class Vehicle(dict):
 
         return self._charging_profile_control("serviceParameters", service_parameters)
 
+#
     def add_departure_timer(self, index, year, month, day, hour, minute):
         """Add a single departure timer with the specified index"""
         departure_timer_setting = {"timers": [
@@ -374,6 +406,7 @@ class Vehicle(dict):
 
         return self._charging_profile_control("departureTimerSetting", departure_timer_setting)
 
+#
     def add_repeated_departure_timer(self, index, schedule, hour, minute):
         """Add repeated departure timer."""
         departure_timer_setting = {"timers": [
@@ -384,12 +417,14 @@ class Vehicle(dict):
 
         return self._charging_profile_control("departureTimerSetting", departure_timer_setting)
 
+#
     def delete_departure_timer(self, index):
         """Delete a single departure timer associated with the specified index"""
         departure_timer_setting = {"timers": [{"timerIndex": index}]}
 
         return self._charging_profile_control("departureTimerSetting", departure_timer_setting)
 
+#
     def add_charging_period(self, index, schedule, hour_from, minute_from, hour_to, minute_to):
         """Add charging period"""
         tariff_settings = {"tariffs": [
@@ -412,6 +447,7 @@ class Vehicle(dict):
 
         return self._charging_profile_control("tariffSettings", tariff_settings)
 
+#
     def _charging_profile_control(self, service_parameter_key, service_parameters):
         """Charging profile API"""
         headers = self.connection.head.copy()
@@ -423,6 +459,7 @@ class Vehicle(dict):
 
         return self.post("chargeProfile", headers, cp_data)
 
+#
     def set_wakeup_time(self, wakeup_time):
         """Set the wakeup time for the specified time (epoch milliseconds)"""
         swu_data = self.authenticate_swu()
@@ -430,12 +467,14 @@ class Vehicle(dict):
         swu_data["startTime"] = wakeup_time
         return self._swu(swu_data)
 
+#
     def delete_wakeup_time(self):
         """Stop the wakeup time"""
         swu_data = self.authenticate_swu()
         swu_data["serviceCommand"] = "END"
         return self._swu(swu_data)
 
+#
     def _swu(self, swu_data):
         """Set the wakeup time for the specified time (epoch milliseconds)"""
         headers = self.connection.head.copy()
@@ -443,22 +482,27 @@ class Vehicle(dict):
         headers["Content-Type"] = "application/vnd.wirelesscar.ngtp.if9.StartServiceConfiguration-v3+json; charset=utf-8"
         return self.post("swu", headers, swu_data)
 
+#
     def enable_service_mode(self, pin, expiration_time):
         """Enable service mode. Will disable at the specified time (epoch millis)"""
         self._prov_command(pin, expiration_time, "protectionStrategy_serviceMode")
 
+#
     def enable_transport_mode(self, pin, expiration_time):
         """Enable transport mode. Will be disabled at the specified time (epoch millis)"""
         self._prov_command(pin, expiration_time, "protectionStrategy_transportMode")
 
+ #
     def enable_privacy_mode(self, pin):
         """Enable privacy mode. Will disable journey logging"""
         self._prov_command(pin, None, "privacySwitch_on")
 
+ #
     def disable_privacy_mode(self, pin):
         """Disable privacy mode. Will enable journey logging"""
         self._prov_command(pin, None, "privacySwitch_off")
 
+ #
     def _prov_command(self, pin, expiration_time, mode):
         """Send prov endpoint commands. Used for service/transport/privacy mode"""
         headers = self.connection.head.copy()
@@ -473,10 +517,12 @@ class Vehicle(dict):
 
         return self.post("prov", headers, prov_data)
 
+ # POST authenticate VHS
     def _authenticate_vhs(self):
         """Authenticate to vhs and get token"""
         return self._authenticate_empty_pin_protected_service("VHS")
 
+ #
     def _authenticate_empty_pin_protected_service(self, service_name):
         data = {
             "serviceName": service_name,
@@ -486,22 +532,27 @@ class Vehicle(dict):
 
         return self.post("users/%s/authenticate" % self.connection.user_id, headers, data)
 
+#
     def authenticate_hblf(self):
         """Authenticate to hblf"""
         return self._authenticate_vin_protected_service("HBLF")
 
+#
     def authenticate_ecc(self):
         """Authenticate to ecc"""
         return self._authenticate_vin_protected_service("ECC")
 
+#
     def authenticate_cp(self):
         """Authenticate to cp"""
         return self._authenticate_vin_protected_service("CP")
 
+#
     def authenticate_swu(self):
         """Authenticate to swu"""
         return self._authenticate_empty_pin_protected_service("SWU")
 
+#
     def _authenticate_vin_protected_service(self, service_name):
         """Authenticate to specified service and return associated token"""
         data = {
@@ -512,22 +563,27 @@ class Vehicle(dict):
 
         return self.post("users/%s/authenticate" % self.connection.user_id, headers, data)
 
+#
     def authenticate_rdl(self, pin):
         """Authenticate to rdl"""
         return self._authenticate_pin_protected_service(pin, "RDL")
 
+#
     def authenticate_rdu(self, pin):
         """Authenticate to rdu"""
         return self._authenticate_pin_protected_service(pin, "RDU")
 
+#
     def authenticate_aloff(self, pin):
         """Authenticate to aloff"""
         return self._authenticate_pin_protected_service(pin, "ALOFF")
 
+#
     def authenticate_prov(self, pin):
         """Authenticate to PROV service"""
         return self._authenticate_pin_protected_service(pin, "PROV")
 
+#
     def _authenticate_pin_protected_service(self, pin, service_name):
         """Authenticate to specified service with the provided PIN"""
         data = {
@@ -538,11 +594,13 @@ class Vehicle(dict):
 
         return self.post("users/%s/authenticate" % self.connection.user_id, headers, data)
 
+#
     def post(self, command, headers, data):
         """Utility command to post data to VHS"""
         return self.connection.post(command, '%s/vehicles/%s' % (IF9_BASE_URL, self.vin),
                                     headers, data)
 
+#
     def get(self, command, headers):
         """Utility command to get vehicle data from API"""
         return self.connection.get(command, '%s/vehicles/%s' % (IF9_BASE_URL, self.vin), headers)
